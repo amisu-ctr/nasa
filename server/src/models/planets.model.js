1e580ef5df4979d866dfe14fafe60cb0e3f51682
amisu <hamisurahman@gmail.com>
2023-11-27 13:54:37 +0000

    csv planet models

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,33 @@
+const {parse} = require('csv-parse')
+
+const planets = [];
+
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+      && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+      && planet['koi_prad'] < 1.6;
+  }
+
+const loadPlanetsData = () => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+      .pipe(parse({
+        comment: '#',
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          savePlanet(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log(err);
+  
+      })
+      .on('end', () => {
+        console.log(isHabitablePlanets.map((planet) => {return planet['kepler_name']}))
+      });
+
+}
+
+module.exports = planets
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,9 +1,13 @@
-const planets = []
+const planets = require('../../models/planets.model')
 
 function getAllPlanets(req, res) {
+    console.log('finally')
     return res.status(200).json(planets)
 }
 
+module.exports = {
+    getAllPlanets
+}
 
 
 
