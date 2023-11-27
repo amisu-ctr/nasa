d34ca10fc2fabfedff49772dd7e6bdcd5845d86a
amisu <hamisurahman@gmail.com>
2023-11-27 19:58:40 +0000

    Morgan

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = 'http://localhost:5555'
 
 async function httpGetPlanets() {
   const response = (await fetch(`${API_URL}/planets`)).json()
--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const path = require("path")
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets.router')
 
@@ -8,8 +10,15 @@ const planetsRouter = require('./routes/planets/planets.router')
 //     origin: 'http://localhost:3000',
 //   }));
 app.use(cors())
+app.use(morgan('combined'));
 
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname,'..','public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join)
+})
 
 app.use(planetsRouter)
 
