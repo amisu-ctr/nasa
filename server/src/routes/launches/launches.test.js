7c37be4927f2f4f54efea880725baa4d3a0d12cf
amisu <hamisurahman@gmail.com>
2023-12-20 14:18:08 +0000

    Mongoose disconnect function to exit tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,15 @@
 const request = require("supertest");
 const app = require("../../app");
-const { mongoConnect } = require("../../services/mongo");
+const {mongoDisconnect, mongoConnect } = require("../../services/mongo");
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect()
-  })
+  });
+
+  afterAll((async() => {
+    await mongoDisconnect()
+  }))
 
   describe("Test GET /launches", () => {
     test("It should respond with 200 success", async () => {
@@ -21,20 +25,20 @@ describe('Launches API', () => {
     const completeLaunchDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      destination: "Kepler-186 f",
+      destination: "Kepler-62 f",
       launchDate: "January 4, 2028",
     };
   
     const launchDataWithoutDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      destination: "Kepler-186 f",
+      destination: "Kepler-62 f",
     };
   
     const launchDataWithInvalidDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      destination: "Kepler-186 f",
+      destination: "Kepler-62 f",
       launchDate: "Zoot",
     };
   
